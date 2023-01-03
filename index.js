0d4af5c9407e760bbab74fd1a2679820c1939dac
Alexander <caminante.msk@gmail.com>
2023-01-02 22:33:35 -0300

    in process

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
 const app = express()
+const addUser = require('./users')
 
 const route = require('./route')
 
@@ -16,6 +17,30 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   }
+})
+
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }) => {
+    socket.join(room)
+
+    const { user } = addUser({ name, room })
+
+    socket.emit('message', {
+      data: { 
+        user: { name: "Admin" },
+        message: `Hey my dear ${user.name}`
+        }
+    })
+    socket.broadcast.to(user.room).emit('message', {
+      data: { 
+        user: { name: "Admin" },
+        message: `${user.name} joined`
+        }
+    })
+  })
+  io.on('disconnect', () => {
+    console.log('Disconnect')
+  })
 })
 
 
